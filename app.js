5092e5fa6ea17463a09c0f1ab99bc339dc78e1cb
JunedSetiawan <junedhayko502@gmail.com>
2023-06-10 11:02:25 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const {PrismaClient} = require('@prisma/client')
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get('/siswa', async (req, res) => {
+    try {
+        const siswa = await prisma.siswa.findMany();
+        if (siswa.length === 0) {
+            res.status(204).send({ message: 'Data siswa kosong' });
+        } else {
+            res.json(siswa);
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Gagal mendapatkan data siswa: ' + err.message });
+    }
+});
+
+
+app.post('/siswa', async (req, res) => {
+    try {
+        const { nama, kelas } = req.body;
+        const result = await prisma.siswa.create({
+            data: {
+                nama: nama,
+                kelas: kelas,
+            }
+        });
+        res.json({
+            message: 'Data siswa berhasil dibuat',
+            result: result
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Gagal membuat data siswa: ' + err.message });
+    }
+});
+
+
+app.put('/siswa/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nama, kelas } = req.body;
+        const updatedSiswa = await prisma.siswa.update({
+            where: { id: Number(id) },
+            data: { nama, kelas }
+        });
+        res.json({
+            message: 'Data berhasil diubah',
+            result: updatedSiswa
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Gagal memperbarui data siswa: ' + err.message });
+    }
+});
+
+app.delete('/siswa/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const siswa = await prisma.siswa.delete({
+            where: { id: Number(id) }
+        });
+        res.json({
+            message: 'Data berhasil dihapus',
+            result: siswa
+        });
+    } catch (err) {
+        res.status(500).json({ message: 'Gagal menghapus data siswa: ' + err.message });
+    }
+});
+
+
+app.listen(3001 || process.env.PORT, ()=>
+    console.log(`Server ready at: http://localhost:3001`)
+)
