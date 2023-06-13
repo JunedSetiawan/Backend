3602a5eb058fdbd2fa5b24a92800684aeef60670
JunedSetiawan <junedhayko502@gmail.com>
2023-06-13 20:14:32 +0700

    deleting testing

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const SiswaRoute = require("./routes/SiswaRoute")
-const path = require("path")
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', SiswaRoute)
-app.use('/tes', function (req, res) {
-    res.sendFile(path.join(__dirname, './src/index.html'));
-  });
-  
+app.use('/api', SiswaRoute)  
   
 app.listen(3001 || process.env.PORT, ()=>
     console.log(`Server ready at: http://localhost:3001`)
