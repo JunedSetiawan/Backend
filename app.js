fd279dd76df5caf788a4036c1e995b1465cfb008
JunedSetiawan <junedhayko502@gmail.com>
2023-06-13 20:12:04 +0700

    on cors

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const SiswaRoute = require("./routes/SiswaRoute")
 const path = require("path")
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 app.use('/api', SiswaRoute)
 app.use('/tes', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './src/index.html'));
   });
   
   
